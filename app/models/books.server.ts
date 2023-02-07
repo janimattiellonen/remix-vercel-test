72e6f5c5b16bcd9579fc02019c447a741ab5f14d
janimattiellonen <janimatti.ellonen@gmail.com>
2023-02-07 20:31:09 +0200

    Testing backend route

---
+++ app/models/books.server.ts
@@ -0,0 +1,14 @@
+export async function getBooks() {
+  return [
+    {
+      id: 42,
+      title: 'Illegalisten',
+      author: 'Anders Jallai'
+    },
+    {
+      id: 43,
+      title: 'Grissly',
+      author: 'Mons Kallentoft'
+    }
+  ]
+}
--- app/routes/books/index.tsx
+++ app/routes/books/index.tsx
@@ -1,8 +1,22 @@
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+
+import {getBooks} from "~/models/books.server";
+import type from "ajv/lib/vocabularies/jtd/type";
+
+export const loader = async () => {
+  const books = await getBooks();
+
+  return json(books)
+}
+
 export default function BooksIndex() {
+  const books = useLoaderData<typeof loader>();
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <h1>Books</h1>
 
+      {books.map ((book) => <li>{book.id}: {book.title}</li>)}
     </div>
   );
 }
